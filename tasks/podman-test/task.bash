91008a4e353a200787451fbd3b5aad4732367aa5
Stephen Simpson <ssimpson89@users.noreply.github.com>
2025-09-25 09:24:55 -0500

    Add newline to HTTP response codes and suppress output for podman-system-generator
    Signed-off-by: Stephen Simpson <ssimpson89@users.noreply.github.com>

--- tasks/podman-test/task.bash
+++ tasks/podman-test/task.bash
@@ -1,11 +1,11 @@
 # Check Nextcloud HTTP response on port 8080
-curl -s -o /dev/null -w '%{http_code}' http://localhost:8080
+curl -s -o /dev/null -w '%{http_code}\n' http://localhost:8080
 
 # Check Nextcloud HTTP response on port 8081 (through systemd user service)
-curl -s -o /dev/null -w '%{http_code}' http://localhost:8081
+curl -s -o /dev/null -w '%{http_code}\n' http://localhost:8081
 
 # Check Podman quadlet service builds successfully
-/usr/lib/systemd/system-generators/podman-system-generator --user --dryrun && echo success || echo fail
+/usr/lib/systemd/system-generators/podman-system-generator --user --dryrun >/dev/null 2>&1 && echo success || echo fail
 
 # Check custom container HTTP response on port 8082
 curl -s http://localhost:8082
@@ -13,4 +13,4 @@ curl -s http://localhost:8082
 #  expect => "Welcome to Rocky"
 
 # Check custom container HTTP response on port 8082 (through podman pod)
-curl -s -o /dev/null -w '%{http_code}' http://localhost:8082
+curl -s -o /dev/null -w '%{http_code}\n' http://localhost:8082
